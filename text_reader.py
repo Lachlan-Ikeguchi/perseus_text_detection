b9a7244c0a34ac5237e497ab9a08a094c64b1de3
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2025-09-06 09:51:21 +1000

    feat(text detection): fragmented code into text detection and reading
    changed text reading model to tessaract, and text detection model to
    east

---
+++ text_reader.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3.13
+
+import pytesseract as tes
+import cv2
+from PIL import Image
+
+tes.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
+reference_image = Image.open('./reference_images/remove_before_flight.jpeg')
+
+print(tes.image_to_string(reference_image))
